f9c180814e05a1b602f7d5f041a407cfc6ed6a57
Michal Dengusiak <tassimulation@gmail.com>
2026-06-05 17:52:11 +0200

    Update SAMOCCTCreateAdjacencyClusterByShells.cs

--- Grasshopper/SAM.Analytical.Grasshopper.OCCT/Component/SAMOCCTCreateAdjacencyClusterByShells.cs
+++ Grasshopper/SAM.Analytical.Grasshopper.OCCT/Component/SAMOCCTCreateAdjacencyClusterByShells.cs
@@ -12,6 +12,7 @@ using SAM.Geometry.Spatial;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace SAM.Analytical.Grasshopper.OCCT
 {
@@ -19,7 +20,7 @@ namespace SAM.Analytical.Grasshopper.OCCT
     {
         public override Guid ComponentGuid => new Guid("d6950fec-cea4-4b48-9099-8943a7765e81");
 
-        public override string LatestComponentVersion => "0.2.0";
+        public override string LatestComponentVersion => "0.3.0";
 
         public SAMOCCTCreateAdjacencyClusterByShells()
           : base("SAMOCCT.CreateAdjacencyClusterByShells", "SAMOCCT.CreateAdjacencyClusterByShells", "Create a SAM AdjacencyCluster from closed shell space volumes using OCCT cell building", "SAM", "OCCT")
@@ -37,6 +38,13 @@ namespace SAM.Analytical.Grasshopper.OCCT
             inputParamManager.AddNumberParameter("silverSpacing_", "silverSpacing_", "OCCT fuzzy tolerance and silver spacing for space computation", GH_ParamAccess.item, Tolerance.MacroDistance);
             inputParamManager.AddNumberParameter("minArea_", "minArea_", "Minimal face area", GH_ParamAccess.item, 0.01);
             inputParamManager.AddNumberParameter("tolerance_", "tolerance_", "Tolerance", GH_ParamAccess.item, Tolerance.Distance);
+            GooSpaceParam gooSpaceParam = new GooSpaceParam();
+            gooSpaceParam.Optional = true;
+            index = inputParamManager.AddParameter(gooSpaceParam, "spaces_", "spaces_", "Optional existing Spaces to match into shell cells. If supplied, matching spaces preserve metadata and names.", GH_ParamAccess.list);
+            inputParamManager[index].DataMapping = GH_DataMapping.Flatten;
+
+            index = inputParamManager.AddTextParameter("names_", "names_", "Optional names for shell-derived seed spaces, used when spaces_ is not supplied or not matched.", GH_ParamAccess.list);
+            inputParamManager[index].Optional = true;
             inputParamManager.AddBooleanParameter("_run", "_run", "Run", GH_ParamAccess.item, false);
         }
 
@@ -52,7 +60,7 @@ namespace SAM.Analytical.Grasshopper.OCCT
             dataAccess.SetData(2, false);
 
             bool run = false;
-            if (!dataAccess.GetData(7, ref run) || !run)
+            if (!dataAccess.GetData(9, ref run) || !run)
             {
                 return;
             }
@@ -100,11 +108,25 @@ namespace SAM.Analytical.Grasshopper.OCCT
             double tolerance = Tolerance.Distance;
             dataAccess.GetData(6, ref tolerance);
 
+            List<Space> inputSpaces = new List<Space>();
+            dataAccess.GetDataList(7, inputSpaces);
+
+            List<string> names = new List<string>();
+            dataAccess.GetDataList(8, names);
+
+            Stopwatch stopwatch_Total = Stopwatch.StartNew();
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
             List<Panel> panels = new List<Panel>();
             List<Space> spaces = new List<Space>();
             int count = 1;
-            foreach (Shell shell in shells)
+            HashSet<Guid> usedSpaceGuids = new HashSet<Guid>();
+            HashSet<string> usedNames = new HashSet<string>();
+
+            for (int i = 0; i < shells.Count; i++)
             {
+                Shell shell = shells[i];
+
                 List<Panel> panels_Temp = global::SAM.Analytical.Create.Panels(shell, silverSpacing, tolerance);
                 if (panels_Temp != null)
                 {
@@ -115,14 +137,58 @@ namespace SAM.Analytical.Grasshopper.OCCT
                 Point3D point3D = shell.InternalPoint3D(silverSpacing, tolerance);
                 if (point3D != null)
                 {
-                    spaces.Add(new Space(string.Format("Cell {0}", count), point3D));
-                    count++;
+                    Space space = null;
+                    if (inputSpaces != null && inputSpaces.Count != 0)
+                    {
+                        List<Shell> shells_Temp = new List<Shell>() { shell };
+                        foreach (Space inputSpace in inputSpaces)
+                        {
+                            if (inputSpace == null || usedSpaceGuids.Contains(inputSpace.Guid) || inputSpace.Location == null)
+                            {
+                                continue;
+                            }
+
+                            List<Shell> spaceShells = global::SAM.Analytical.Query.SpaceShells(shells_Temp, inputSpace.Location, silverSpacing, tolerance);
+                            if (spaceShells != null && spaceShells.Count != 0)
+                            {
+                                space = new Space(inputSpace, inputSpace.Name, point3D);
+                                usedSpaceGuids.Add(inputSpace.Guid);
+                                break;
+                            }
+                        }
+                    }
+
+                    if (space == null)
+                    {
+                        string name = null;
+                        if (names != null && i < names.Count && !string.IsNullOrWhiteSpace(names[i]))
+                        {
+                            name = names[i];
+                        }
+
+                        if (string.IsNullOrWhiteSpace(name))
+                        {
+                            do
+                            {
+                                name = string.Format("Cell {0}", count);
+                                count++;
+                            }
+                            while (usedNames.Contains(name));
+                        }
+
+                        space = new Space(name, point3D);
+                    }
+
+                    usedNames.Add(space.Name);
+                    spaces.Add(space);
                 }
             }
 
             diagnostics.Add(string.Format("SAM_OCCT_ANALYTICAL_SHELL_PANELS: Extracted {0} panel(s) and {1} seed space(s) from {2} shell(s).", panels.Count, spaces.Count, shells.Count));
+            diagnostics.Add(string.Format("SAM_OCCT_TIMING_PANEL_EXTRACTION: {0:0.000}s.", stopwatch.Elapsed.TotalSeconds));
 
             Log log = new Log();
+            stopwatch.Restart();
             AdjacencyCluster adjacencyCluster = global::SAM.Analytical.OCCT.Create.AdjacencyCluster(
                 spaces,
                 panels,
@@ -133,12 +199,14 @@ namespace SAM.Analytical.Grasshopper.OCCT
                 minArea: minArea,
                 maxDistance: maxDistance,
                 maxAngle: maxAngle);
+            diagnostics.Add(string.Format("SAM_OCCT_TIMING_OCCT_AND_ADJACENCY: {0:0.000}s.", stopwatch.Elapsed.TotalSeconds));
 
             if (cellComplexResult?.Diagnostics != null)
             {
                 diagnostics.AddRange(cellComplexResult.Diagnostics.Select(x => x.ToString()));
             }
 
+            stopwatch.Restart();
             if (adjacencyCluster == null)
             {
                 diagnostics.Add("SAM_OCCT_ANALYTICAL_SHELL_REBUILD_FAILED: OCCT could not create a valid adjacency cluster from panels extracted from the supplied shells.");
@@ -151,6 +219,8 @@ namespace SAM.Analytical.Grasshopper.OCCT
                 adjacencyCluster.UpdatePanelTypes(elevationGround);
                 adjacencyCluster.SetDefaultConstructionByPanelType();
             }
+            diagnostics.Add(string.Format("SAM_OCCT_TIMING_POST_PROCESS: {0:0.000}s.", stopwatch.Elapsed.TotalSeconds));
+            diagnostics.Add(string.Format("SAM_OCCT_TIMING_TOTAL: {0:0.000}s.", stopwatch_Total.Elapsed.TotalSeconds));
 
             dataAccess.SetData(0, adjacencyCluster == null ? null : new GooAdjacencyCluster(adjacencyCluster));
             dataAccess.SetDataList(1, diagnostics);
